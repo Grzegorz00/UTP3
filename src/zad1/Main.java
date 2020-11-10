094ee5fcc719dcba7f830bb070eb9cd9443b7da1
Grzegorz00 <g.swiecickii@gmail.com>
2020-11-10 11:21:23 +0100

    Add MyFunction interface (to avoid try catch)

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -14,8 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class Main {
   public static void main(String[] args) {
